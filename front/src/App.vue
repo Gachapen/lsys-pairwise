a26e1d50321f854fd174fed5ea384133adc264c0
Magnus Bjerke Vik <mbvett@gmail.com>
2017-11-12 20:57:36 +0100

    [front] Add consent page as index page

--- front/src/App.vue
+++ front/src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
 import VueRouter from 'vue-router'
+import Consent from './components/Consent'
 import Register from './components/Register'
 import Task from './components/Task'
 import Result from './components/Result'
@@ -14,6 +15,10 @@ const router = new VueRouter({
   routes: [
     {
       path: '/',
+      component: Consent,
+    },
+    {
+      path: '/register',
       component: Register,
       props: route => ({
         initialTask: route.query.task,
@@ -46,17 +51,66 @@ html, body, #app
   margin: 0
   padding: 0
   background: #282828
-
+  color: white
 
 html
   box-sizing: border-box
 *, *:before, *:after
   box-sizing: inherit
 
+a
+  color: white
+
+button, a[role="button"]
+  padding: 8px
+  font-size: 12pt
+  background: #666666
+  border: 0
+  border-radius: 3px
+  color: white
+  text-decoration: none
+
+  &:hover:not(.disabled):not(:disabled)
+    cursor: pointer
+
+  &:disabled, &.disabled
+    background: #363636
+    color: #777
+
+.disabled
+  pointer-events: none
+
+section
+  max-width: 800px
+  margin: 0 auto
+
+  >h2
+    text-align: center
+  >p
+    text-align: left
+
+section.questionaire
+  >section
+    margin-top: 40px
+    margin-bottom: 34px
+
+    >h4
+      text-align: left
+
+    textarea
+      width: 100%
+      height: 100px
+
 #app
   font-family: 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   text-align: center
-  color: #2c3e50
+
+  >*
+    overflow: auto
+
+    >header
+      >h2
+        text-align: center
 </style>
---
+++ front/src/components/Consent.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .consent
+    section.consent
+      h2 Evaluating Generated Virtual Plants
+      p.
+        You are being asked to participate in a study investigating ways to improve the process of generating aesthetically pleasing virtual plants that can be used in virtual environment such as games, and be combined into new virtual plants.
+      p.
+        The program that generates the plants needs to evaluate how aesthetically pleasing the plants are in order to distinguish good plants from bad plants.
+        Your contribution in this study will be on evaluating how well this program evaluates the plants.
+      p.
+        If you agree to participate, you will complete three activities: a pre-questionaire, evaluating pairs of plants, and a post-questionaire.
+        The whole process is expected to take around 10 minutes.
+      p.
+        All data collected is anonymous and confidential.
+      p.
+        Your participation is voluntary and you may withdraw at any time during the study without prejudice.
+      p.
+      p
+        input(type='checkbox' id='agree' v-model='agree')
+        label(for='agree').
+          I have read the above and understand the nature of this study.
+          I understand that by agreeing to participate in this study I have not waived any legal or human right and that I may contact the researcher (Magnus Bjerke Vik #[a(href='mailto:magnusbv@stud.ntnu.no') &lt;magnusbv@stud.ntnu.no&gt;]) at any time.
+          I agree to participate in this study.
+          I understand that I may refuse to participate or I may withdraw from the study at any time without prejudice.
+      p
+        button(:disabled='!agree' @click='$router.push({ path: "register", query: $router.currentRoute.query })') Participate
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      agree: false,
+    }
+  },
+}
+</script>
+
+<style scoped lang="sass">
+</style>
