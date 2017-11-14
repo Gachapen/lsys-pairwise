1cbf2b9c33719144375152c0282a5619a5279381
Magnus Bjerke Vik <mbvett@gmail.com>
2017-11-14 18:27:29 +0100

    [front] Add data collection and recommendations in consent

--- front/src/components/Consent.vue
+++ front/src/components/Consent.vue
@@ -11,17 +11,26 @@
         If you agree to participate, you will complete three activities: a pre-questionnaire, evaluating pairs of plants, and a post-questionnaire.
         The whole process is expected to take around 10 minutes.
       p.
+        Data that you input in fields will be collected.
+        Additionally, some data will be automatically collected, limited to your browser name and version, plant video dimensions, submission times and if fullscreen is enabled or not.
+        If you received a URL shared by another participant, who this participant was will also be collected.
         All data collected is anonymous and confidential.
       p.
         Your participation is voluntary and you may withdraw at any time during the study without prejudice.
       p.
       p
         input(type='checkbox' id='agree' v-model='agree')
-        label(for='agree').
-          I have read the above and understand the nature of this study.
-          I understand that by agreeing to participate in this study I have not waived any legal or human right and that I may contact the researcher (Magnus Bjerke Vik #[a(href='mailto:magnusbv@stud.ntnu.no') &lt;magnusbv@stud.ntnu.no&gt;]) at any time.
-          I agree to participate in this study.
-          I understand that I may refuse to participate or I may withdraw from the study at any time without prejudice.
+        label(for='agree')
+          b.
+            I have read the above and understand the nature of this study.
+            I understand that by agreeing to participate in this study I have not waived any legal or human right and that I may contact the researcher (Magnus Bjerke Vik #[a(href='mailto:magnusbv@stud.ntnu.no') &lt;magnusbv@stud.ntnu.no&gt;]) at any time.
+            I agree to participate in this study.
+            I understand that I may refuse to participate or I may withdraw from the study at any time without prejudice.
+      p.
+        It is recommended to use a device with a large screen, such as a desktop PC, laptop PC or tablet to observe the plants in more detail.
+        You should also use a modern browser to avoid technical issues.
+        If, during the plant evaluation, you do not see two videos of plants side by side or the plants do not rotate, please try a different browser, or withdraw from the study as it may bias the results.
+        Please contact the researcher (Magnus Bjerke Vik #[a(href='mailto:magnusbv@stud.ntnu.no') &lt;magnusbv@stud.ntnu.no&gt;]) if you notice any issues.
       p
         button(:disabled='!agree' @click='$router.push({ path: "register", query: $router.currentRoute.query })') Participate
 </template>
