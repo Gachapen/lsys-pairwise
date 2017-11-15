50e13af4989c3a39985960a32f78d68b4c086e7f
Magnus Bjerke Vik <mbvett@gmail.com>
2017-11-15 21:37:07 +0100

    [front] Rephrase consent to be simpler and easier to read

--- front/src/components/Consent.vue
+++ front/src/components/Consent.vue
@@ -3,10 +3,11 @@
     section.consent
       h2 Evaluating Generated Virtual Plants
       p.
-        You are being asked to participate in a study investigating ways to improve the process of generating aesthetically pleasing virtual plants that can be used in virtual environment such as games, and be combined into new virtual plants.
+        You are being asked to participate in a study investigating ways to improve the process of creating computer generated plants.
+        These plants could be used in virtual environments, such as video games, movies and simulations.
       p.
-        The program that generates the plants needs to evaluate how aesthetically pleasing the plants are in order to distinguish good plants from bad plants.
-        Your contribution in this study will be on evaluating how well this program evaluates the plants.
+        The computer program that generates the plants needs to be able to distinguish good plants from bad plants.
+        Your contribution in this study will be on evaluating how well this program does exactly that.
       p.
         If you agree to participate, you will complete three activities: a pre-questionnaire, evaluating pairs of plants, and a post-questionnaire.
         The whole process is expected to take around 10 minutes.
@@ -17,17 +18,13 @@
         All data collected is anonymous and confidential.
       p.
         Your participation is voluntary and you may withdraw at any time during the study without prejudice.
-      p.
+        You may contact the researcher (Magnus Bjerke Vik #[a(href='mailto:magnusbv@stud.ntnu.no') &lt;magnusbv@stud.ntnu.no&gt;]) at any time.
       p
-        input(type='checkbox' id='agree' v-model='agree')
+        input(type='checkbox' id='agree' v-model='agree' style='margin-right: 10px')
         label(for='agree')
-          b.
-            I have read the above and understand the nature of this study.
-            I understand that by agreeing to participate in this study I have not waived any legal or human right and that I may contact the researcher (Magnus Bjerke Vik #[a(href='mailto:magnusbv@stud.ntnu.no') &lt;magnusbv@stud.ntnu.no&gt;]) at any time.
-            I agree to participate in this study.
-            I understand that I may refuse to participate or I may withdraw from the study at any time without prejudice.
+          b I have read the above, understand the nature of the study, and agree to participate.
       p.
-        It is recommended to use a device with a large screen, such as a desktop PC, laptop PC or tablet to observe the plants in more detail.
+        It is recommended to use a device with a large screen, such as a desktop computer, laptop or tablet to observe the plants in more detail.
       p
         button(:disabled='!agree' @click='$router.push({ path: "register", query: $router.currentRoute.query })') Participate
 </template>
